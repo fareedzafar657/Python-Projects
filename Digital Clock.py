efd9c0eecd09b348c42a515d6d82272f5fc51a21
fareedzafar657 <69139089+fareedzafar657@users.noreply.github.com>
2023-02-04 21:13:39 +0500

    Create Digital Clock.py

---
+++ Digital Clock.py
@@ -0,0 +1,22 @@
+from tkinter import Tk
+from tkinter import Label
+import time
+
+
+def update_clock():
+    current_time = time.strftime("%I:%M:%S:%p")  # %I for 12 hours format
+    clock.configure(text=current_time)
+    clock.after(100, update_clock)
+
+
+window = Tk()
+window.title("Digital Clock")
+window.geometry("650x200")
+window.config(bg="#232b2b")
+
+clock = Label(window, font=("Orbitron", 60), bg="#232b2b", fg="#f5f7f7",)
+clock.pack(fill="both", expand=1)
+clock.place(x=325, y=100, anchor="center")
+
+update_clock()
+window.mainloop()
